4664e9f19be2c98e40289bba287633fe57c0062b
edoardo1710 <edoardo.bottosso@gmail.com>
2024-12-03 18:54:20 +0100

    Preghiamo

---
+++ Lezione_5/Esercizio_2.py
@@ -0,0 +1,47 @@
+print('Esercizio 2: estendere l oggetto CSVFile')
+
+class CSVFile ():
+    
+    def __init__(self,name):
+        
+        self.name = name
+    
+    def __str__(self):
+        
+        return 'File: "{}"'.format(self.name)
+    
+    def get_data(self):
+
+        try:
+            file_parametro = open(self.name)
+            parametro_come_stringa = file_parametro.read()
+        except FileNotFoundError as e:
+            print("Errore")
+            print("Verrà utilizzato il file 'shampoo_sales.csv'")
+            phrases = []
+            with open('shampoo_sales.csv', 'r') as my_file:
+                for line in my_file:
+                    elements = line.strip().split(',')
+                    if elements[0] != 'Date':
+                        phrases.append(elements)
+            return phrases
+        else:
+            phrases = []
+            with open(self.name, 'r') as my_file:
+                for line in my_file:
+                    elements = line.strip().split(',')
+                    if elements[0] != 'Date':
+                        phrases.append(elements)
+            return phrases
+
+class NumericalCSVFile(CSVFile):
+
+    def __init__(self,name):
+
+        super().__init__(name)
+
+    def __str__(self):
+        
+        return super().__str__
+    
+    
