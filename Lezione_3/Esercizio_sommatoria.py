5e416829445d0aecf3f57b542f74670e6743c2d0
edoardo1710 <edoardo.bottosso@gmail.com>
2024-12-03 15:50:01 +0100

    Spero di non aver cancellato tutto

--- Lezione_3/Esercizio_sommatoria.py
+++ Lezione_3/Esercizio_sommatoria.py
@@ -1,19 +1,10 @@
-# print('Esercizio di prova: definire una funzione che sommi i valori di tutte le vendite presenti nel file')
-
-values = []
-my_file = open('shampoo_sales.csv', 'r')
-for line in my_file:
-    elements = line.strip().split(',')
-    if elements[0] != 'Date':
-        date = elements[0]
-        value = float(elements[1])
-        values.append(value)
-my_file.close()
-
-def sum_csv(values):
-    total = 0
-    for i in values:
-        total += i
-    return total
-
-# print(f'Valori vendite: {sum_csv(values)}')
+def sum_csv(path_file):
+    tot = 0
+    with open(path_file, 'r') as my_file:
+        for line in my_file:
+            elements = line.strip().split(',')
+            if elements[0] == 'Date':
+                tot += float(elements[1])
+    return tot
+osso = sum_csv('shampoo_sales.csv')
+print(f'{osso}')
