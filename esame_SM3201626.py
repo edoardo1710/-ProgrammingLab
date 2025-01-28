2a8454f03fff5257156b37ba657a3b682734f812
edoardo1710 <edoardo.bottosso@gmail.com>
2025-01-28 14:21:22 +0100

    Speriamo ultimo controllo

--- esame_SM3201626.py
+++ esame_SM3201626.py
@@ -8,11 +8,15 @@ class CSVTimeSeriesFile():
         """Costruttore
 
         Parametri: 
-            file_name (lista): elenco data (YYYY-MM) e numero di passeggeri (in migliaia)
+            file_name (path del file): elenco data (YYYY-MM) e numero di passeggeri (in migliaia)
         """
-        
+
+        if not isinstance(file_name, str):
+            raise ExamException("Il parametro passato non è del tipo richiuesto, ci si aspetta una stringa (path del file).")
+
         self.file_name = file_name
         
+
     def get_data(self):
         """Metodo 'get_data'
 
@@ -31,34 +35,44 @@ class CSVTimeSeriesFile():
         except:
             raise ExamException("Errore nell'apertura del file")
         
+
+        try:
+            data = file.read().splitlines()
+        except:
+            raise ExamException("Errore nella lettura del file.")
+        
         time_series_list = []
 
-        with open(self.file_name, 'r') as my_file:
+        for line in data:
+            elements = line.strip().split(',')
 
-            for line in my_file:
-                elements = line.strip().split(',')
+            if elements[0] != 'date':
+                control_date = elements[0]
+                year_str, month_str = elements[0][:4], elements[0][5:7]
 
-                if elements[0] != 'date':
-                    control_date = elements[0]
+                if year_str.isdigit() is False:
+                    raise ExamException("Valore anno non valido")
+                
+                if month_str.isdigit() is False:
+                    raise ExamException("Valore mese non valido")
+                
+                year, month = int(year_str), int(month_str)
 
-                    if control_date[:4].isdigit() is False:
-                        raise ExamException("Valore anno non valido")
-                    
-                    if control_date[5:7].isdigit() is False:
-                        raise ExamException("Valore mese non valido")
-                    
-                    control_month = int(control_date[5:7])
-                    if (control_month < 1) or (control_month > 12):
-                        raise ExamException("Mese non esistente")
+                if (month < 1) or (month > 12):
+                    raise ExamException("Mese non esistente")
 
-                    if elements[1].isdigit():
-                        elements[1] = int(elements[1])
-                        time_series_list.append(elements)
-        
-        file.close()
+                if year < 1903:
+                    raise ExamException("Gli aerei in quell'anno non sono ancora stati inventanti.")
+
+                if elements[1].isdigit():
+                    time_series_list.append([elements[0], int(elements[1])])
+            
+            file.close()
+
         return time_series_list
 
-def media(list):
+
+def media(data):
     """Funzione 'media'
 
     Parametri:
@@ -68,9 +82,11 @@ def media(list):
         Media della lista
     
     """
-    media = sum(list)/len(list)
-    
-    return media
+    if not isinstance(data, list):
+        raise ExamException("Il parametro passato non è del tipo richiuesto, ci si aspetta una lista.")
+
+
+    return (sum(data) / len(data))
 
 def compute_variations(time_series, first_year, last_year):
     """Funzione 'compute_variations'
@@ -124,33 +140,33 @@ def compute_variations(time_series, first_year, last_year):
         if int(check_years_2[:4]) < first_year:
             i += 1
         
-        if int(check_years_2[:4]) == first_year:
+        elif int(check_years_2[:4]) == first_year:
 
             while int(check_years_2[:4]) == first_year:
-                list_control.append(check_years_1[1])
+                list_control.append(time_series[i][1])
                 i += 1
+
                 try:
-                    check_years_1 = time_series[i]
-                    check_years_2 = check_years_1[0]
+                    check_years_2 = time_series[i][0]
                 except IndexError:
                     break
             
             years_averages.append([first_year, media(list_control)])
             first_year += 1
         
-        if int(check_years_2[:4]) > first_year:
+        elif int(check_years_2[:4]) > first_year:
             years_averages.append([first_year, 0])
             first_year += 1
 
     my_dictionary = {}
 
     if len(years_averages) == 1:
-        key = f"{first_year-1}"
-        control = years_averages[0]
-        my_dictionary[key] = control[1]
+        key = f"{first_year - 1}"
+        my_dictionary[key] = years_averages[0][1]
     else:
         i = 0
-        while i <= (len(years_averages)-1):
+
+        while i <= (len(years_averages) - 1):
             time_controller_1 = years_averages[i]
 
             try:
@@ -158,26 +174,59 @@ def compute_variations(time_series, first_year, last_year):
             except IndexError:
                 break
             
+            if time_controller_1[1] == 0:
+                j = i+1
+                
+                try:
+                    time_controller_1 = years_averages[j]
+                except IndexError:
+                    break
+
+                while time_controller_1[1] == 0:
+                    j += 1
+                    
+                    try:
+                        time_controller_1 = years_averages[j]
+                    except IndexError:
+                        break
 
             if time_controller_2[1] != 0:
                 diff = time_controller_2[1] - time_controller_1[1]
-                diff = round(diff, 2)
                 key = f"{time_controller_1[0]}-{time_controller_2[0]}"
-                my_dictionary[key] = diff
+                my_dictionary[key] = round(diff, 2)
                 i += 1
 
             if time_controller_2[1] == 0:
                 j = i+1
-                time_controller_2 = years_averages[j+1]
+
+                try:
+                    time_controller_2 = years_averages[j+1]
+                except IndexError:
+                    break    
 
                 while time_controller_2[1] == 0:
                     j += 1
-                    time_controller_2 = years_averages[j+1]
+
+                    try:
+                        time_controller_2 = years_averages[j+1]
+                    except IndexError:
+                        break
                 
                 diff = time_controller_2[1] - time_controller_1[1]
-                diff = round(diff, 2)
                 key = f"{time_controller_1[0]}-{time_controller_2[0]}"
-                my_dictionary[key] = diff
+                my_dictionary[key] = round(diff, 2)
                 i = j+1
 
     return my_dictionary
+
+name = 'data.csv'
+time_series_file = CSVTimeSeriesFile(name)
+time_series = time_series_file.get_data()
+
+dict = compute_variations(time_series, "1949", "1962")
+
+print("{")
+for k,v in dict.items():
+    print(f"    {k}: {v}")
+print("}")
+
